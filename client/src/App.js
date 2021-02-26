c6b45961bb5174ab6974061dd5b1c3c221af4324
petercolella <peter.joseph.colella@gmail.com>
2021-02-26 18:37:10 -0500

    initial setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,39 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+const App = () => {
+  const [place, setPlace] = useState("");
+  const [image, setImage] = useState("https://via.placeholder.com/200");
+
+  const handleSubmit = () => {
+    console.log(place);
+  };
+
+  const handleChange = (e) => {
+    setPlace(e.target.value);
+  };
+
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to React</h2>
       </div>
-    );
-  }
-}
+      <div className="App-intro">
+        <input
+          name="place"
+          placeholder="Enter a place"
+          value={place}
+          onChange={handleChange}
+        />
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
+      <div>
+        <img src={image} alt="selected place" />
+      </div>
+    </div>
+  );
+};
 
 export default App;
