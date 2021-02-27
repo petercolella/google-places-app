6a4562ef29079ff0386cc23e0f298e9212059f8d
petercolella <peter.joseph.colella@gmail.com>
2021-02-26 19:49:52 -0500

    updates /api/placeImages post route in server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const fetch = require("node-fetch");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -13,9 +15,31 @@ if (process.env.NODE_ENV === "production") {
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.post("/api/placeImages", (req, res) => {
-  console.log(req.body);
-  res.send({ success: true });
+app.post("/api/placeImages", async (req, res) => {
+  const response = await fetch(
+    `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${req.body.place}&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=${process.env.PLACES_API_KEY}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  const result = await response.json();
+  const photo_reference = result.candidates[0].photos[0].photo_reference;
+
+  const photoResponse = await fetch(
+    `https://maps.googleapis.com/maps/api/place/photo?photoreference=${photo_reference}&key=${process.env.PLACES_API_KEY}&maxwidth=400&maxheight=400`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  res.json({ success: true, imageUrl: photoResponse.url });
 });
 
 app.get("*", function (req, res) {
